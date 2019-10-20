ba14bd2a6040ff521d56b9bd619dda636cdc7ed1
root <root@ip-172-31-26-242.us-east-2.compute.internal>
2019-10-20 17:09:04 +0000

    typo corrected in run-stack.sh

--- run-stack.sh
+++ run-stack.sh
@@ -7,6 +7,6 @@ aws cloudformation $ACTION \
 	--capabilities CAPABILITY_NAMED_IAM \
 	--parameters \
 	ParameterKey=DockerImage,ParameterValue=679721405862.dkr.ecr.us-east-2.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
-	ParameterKey=VPC,ParameterValue=vpc-964ebefd
+	ParameterKey=VPC,ParameterValue=vpc-964ebefd \
 	ParameterKey=Cluster,ParameterValue=default \
 	ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-2:679721405862:loadbalancer/app/production-website/d5dc81ce06c11b7b
