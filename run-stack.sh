9a595466a58d4c39e7ddac7c5af0158a5c78341c
root <root@ip-172-31-26-242.us-east-2.compute.internal>
2019-10-20 16:51:56 +0000

    adding ecs deploy

---
+++ run-stack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aws cloudformation $ACTION \
+	--region us-east-1 \
+	--stack-name $STACK_NAME \
+	--template-body file://service.yaml \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--parameters \
+	ParameterKey=DockerImage,ParameterValue=679721405862.dkr.ecr.us-east-2.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
+	ParameterKey=VPC,ParameterValue=vpc-964ebefd
+	ParameterKey=Cluster,ParameterValue=default \
+	ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-2:679721405862:loadbalancer/app/production-website/d5dc81ce06c11b7b
